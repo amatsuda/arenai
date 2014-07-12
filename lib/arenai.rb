7ed550337bb4f12bf310ed4a256c842cd7fec161
Akira Matsuda <ronnie@dio.jp>
2014-07-13 03:31:51 +0900

    Implement find(id)

--- lib/arenai.rb
+++ lib/arenai.rb
@@ -1,5 +1,24 @@
 require "arenai/version"
 
 module Arenai
-  # Your code goes here...
+  module Base
+    def find(*ids)
+      return super unless ids.length == 1
+      return super if block_given? ||
+                      primary_key.nil? ||
+                      default_scopes.any? ||
+                      columns_hash.include?(inheritance_column) ||
+                      ids.first.kind_of?(Array)
+      id = ids.first
+      return super if !((Fixnum === id) || (String === id))
+
+      # SELECT "users".* FROM "users" WHERE "users"."id" = ?  [["id", 1]]
+      find_by_sql("SELECT #{quoted_table_name}.* FROM #{quoted_table_name} WHERE #{quoted_table_name}.#{connection.quote_column_name primary_key} = ?", [[User.columns_hash[primary_key], id]]).first
+    end
+end
+
+ActiveSupport.on_load :active_record do
+  class << ActiveRecord::Base
+    prepend Arenai::Base
+  end
 end
