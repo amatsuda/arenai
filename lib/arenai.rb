aab18016812b0e661dbde3f49db16e685a4b3b5b
Akira Matsuda <ronnie@dio.jp>
2015-08-06 04:08:36 +0900

    normal select

--- lib/arenai.rb
+++ lib/arenai.rb
@@ -28,49 +28,10 @@ module Arenai
       @arenai_values ||= Hash.new.tap {|h| h[:where], h[:order] = [], []}
     end
 
-    def where(opts = :chain, *rest)
-      if (opts == :chain) || opts.blank?
-        super
-      else
-        case opts
-        when String, Array
-          condition = @klass.send(:sanitize_sql, rest.empty? ? opts : ([opts] + rest))
-          @arenai_values[:where] << "(#{condition})"
-        when Hash
-          opts.each_pair do |k, v|
-            case v
-            #TODO
-            when Array, Hash, ActiveRecord::Base
-              # do nothing
-            when nil
-              @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} IS NULL"
-            else
-              @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} = #{connection.quote v}"
-            end
-          end
-        end
-        super
-      end
-    end
+    def to_sql
+      return super if where_values.any? || from_value || joins_values.any? || includes_values.any? || eager_load_values.any? || preload_values.any? || references_values.any? || lock_value
 
-    def order(*args)
-      args.each do |o|
-        case o
-        when String
-          @arenai_values[:order] << o
-        when Symbol
-          @arenai_values[:order] << "#{quoted_table_name}.#{connection.quote_column_name o}"
-        end
-      end
-      super
-    end
-
-    private def exec_queries
-      return super if from_value || joins_values.any? || includes_values.any? || eager_load_values.any? || preload_values.any? || references_values.any? || lock_value
-      return super if where_values.size != @arenai_values[:where].size
-      return super if order_values.size != @arenai_values[:order].size
-
-      sql = 'sELECT'
+      sql = 'SELECT'
       sql << ' DISTINCT' if distinct_value
       sql << (select_values.any? ? " #{select_values.join(', ')}" : " #{quoted_table_name}.*")
       sql << " FROM #{quoted_table_name}"
@@ -80,7 +41,82 @@ module Arenai
       sql << " ORDER BY #{@arenai_values[:order].join(', ')}" if @arenai_values[:order].any?
       sql << " LIMIT #{limit_value}" if limit_value
       sql << " OFFSET #{offset_value}" if offset_value
-      @records = @klass.find_by_sql sql, []
+      sql
+    end
+
+    private def build_relation_from_arenai_values
+      ret = self
+      if @arenai_values[:where].any?
+        @arenai_values[:where].each do |w|
+          ret = ret.arenai_original(:where, w)
+        end
+      end
+      #TODO handle multiple values
+      ret = ret.order(@arenai_values[:order]) if @arenai_values[:order].any?
+      ret
+    end
+
+    #TODO `or` method
+    def where(opts = :chain, *rest)
+      if (opts == :chain) || opts.blank?
+        build_relation_from_arenai_values.merge(super)
+      else
+        case opts
+        when String, Array
+          condition = @klass.send(:sanitize_sql, rest.empty? ? opts : ([opts] + rest))
+          @arenai_values[:where] << "(#{condition})"
+          self
+        when Hash
+          opts.each_pair do |k, v|
+            case v
+            when Array
+              #FIXME handle nil
+              if v.include? nil
+                return build_relation_from_arenai_values.arenai_original(:where, opts)
+              end
+              compact_v = v.compact
+              if compact_v.one?
+                @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} = #{connection.quote compact_v.first}"
+              else
+                @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} IN (#{compact_v.map {|vv| connection.quote vv}.join(', ')})"
+              end
+            when ActiveRecord::Base
+              @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} = #{v.id}"
+            #TODO
+            when Hash, ActiveRecord::Base
+              return build_relation_from_arenai_values.arenai_original(:where, opts)
+            when nil
+              @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} IS NULL"
+            else
+              @arenai_values[:where] << "#{quoted_table_name}.#{connection.quote_column_name k} = #{connection.quote v}"
+            end
+          end
+          self
+        else
+          build_relation_from_arenai_values.arenai_original(:where, opts)
+        end
+      end
+    end
+
+    def order(*args)
+      if args.all? {|a| (String === a) || (Symbol === a) }
+        args.each do |o|
+          case o
+          when String
+            @arenai_values[:order] << o
+          when Symbol
+            @arenai_values[:order] << "#{quoted_table_name}.#{connection.quote_column_name o}"
+          end
+        end
+        self
+      else
+        build_relation_from_arenai_values.arenai_original(:order, args)
+      end
+    end
+
+    private def exec_queries
+      return super if where_values.any? || from_value || joins_values.any? || includes_values.any? || eager_load_values.any? || preload_values.any? || references_values.any? || lock_value
+      @records = @klass.find_by_sql to_sql, []
 
       @records.each { |record| record.readonly! } if readonly_value
 
@@ -88,10 +124,8 @@ module Arenai
       @records
     end
 
-    private def rebuild_relation
-      @arenai_values.inject(self) do |rel, (key, value)|
-        ActiveRecord::Relation.instance_method(key).super_method().bind(rel).call(value)
-      end
+    protected def arenai_original(meth, opts)
+      ActiveRecord::Relation.instance_method(meth).super_method().bind(self).call(opts)
     end
   end
 end
